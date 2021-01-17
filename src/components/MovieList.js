df53ab158852f8144f160e126c53e1718f620bdb
JP Meyers <johnpmeyers20@yahoo.com>
2021-01-17 17:23:19 -0500

    added missing key value to each item from map in MovieList

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,11 +3,11 @@ import React from 'react';
 const MovieList = (props) => {
   return (
     <>
-      {props.movies.map((movie, index) => {
-        <div className="image-container d-flex justify-content-start m-3">
+      {props.movies.map((movie, index) => (
+        <div className="image-container d-flex justify-content-start m-3" key={movie.imdbID}>
           <img src={movie.Poster} alt="movie"></img>
         </div>
-      })}
+      ))}
     </>
   )
 }

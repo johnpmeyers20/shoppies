3bfe7298972263da5b3d25773e41de3cb82d85a6
JP Meyers <johnpmeyers20@yahoo.com>
2021-01-17 17:47:54 -0500

    added Header and SearchBox components which are rendered in the same div within App

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React, {useState, useEffect} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MovieList from './components/MovieList';
+import Header from './components/Header';
+import SearchBox from './components/SearchBox';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
 
   const getMovieRequest = async () => {
     const url = `http://www.omdbapi.com/?apikey=1f020500&s=star wars`;
@@ -22,6 +25,10 @@ const App = () => {
 
   return (
     <div className="container-fluid shoppies">
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <Header heading='Movies'/>
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
+      </div>
       <div className="row">
         <MovieList movies={movies}/>
       </div>

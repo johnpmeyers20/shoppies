3c28aebb1272fa771f08860dc8e0a7b30279ed5c
JP Meyers <johnpmeyers20@yahoo.com>
2021-01-17 18:38:16 -0500

    added nominate div with hover affect

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -6,6 +6,9 @@ const MovieList = (props) => {
       {props.movies.map((movie, index) => (
         <div className="image-container d-flex justify-content-start m-3" key={movie.imdbID}>
           <img src={movie.Poster} alt="movie"></img>
+          <div className="overlay d-flex align-items-center justify-content-center">
+            Nominate
+          </div>
         </div>
       ))}
     </>

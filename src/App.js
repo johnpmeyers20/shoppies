366e381304459673c00a6540f0194ab8a82b999a
johnpmeyers20 <johnpmeyers20@yahoo.com>
2021-05-07 16:59:41 -0400

    another guard operator

--- src/App.js
+++ src/App.js
@@ -93,7 +93,7 @@ const App = () => {
       </div>
       { movies.length === 0
         ?
-          nominees.length > 0 ? <p>Welcome back! Continue nominating films by searching with the search box in the upper right-hand corner</p> : <CallToAction /> 
+          nominees && nominees.length > 0 ? <p>Welcome back! Continue nominating films by searching with the search box in the upper right-hand corner</p> : <CallToAction /> 
         :
         null
       }

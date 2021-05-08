b54c42e6c353ea3772f1b2431077ac3f68b61947
johnpmeyers20 <johnpmeyers20@yahoo.com>
2021-05-08 15:12:10 -0400

    took all logic from MovieList component and placed it outside the render method into a variable

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -4,27 +4,30 @@ import React from 'react';
 const MovieList = (props) => {
   const AddNominee = props.addNominee;
   const nominees = props.nominees;
+
+  const movieList = props.movies.map((movie, index) => (
+    <div className="container" key={movie.imdbID}>
+      <div className="image-container d-flex justify-content-center m-1">
+        <img src={movie.Poster} alt="movie" />
+        { nominees && nominees.length < 5 ?
+          <div
+            onClick={() => props.handleNominee(movie)}
+            className="overlay d-flex align-items-center justify-content-center">
+            <AddNominee />
+          </div>
+          :
+          console.log('cray cray')
+        } 
+      </div>
+      <div className="movie-info">
+        <h5>{movie.Title} ({movie.Year})</h5>
+      </div>
+    </div>
+  ))
+
   return (
     <>
-      {props.movies.map((movie, index) => (
-        <div className="container" key={movie.imdbID}>
-          <div className="image-container d-flex justify-content-center m-1">
-            <img src={movie.Poster} alt="movie" />
-            { nominees && nominees.length < 5 ?
-              <div
-                onClick={() => props.handleNominee(movie)}
-                className="overlay d-flex align-items-center justify-content-center">
-                <AddNominee />
-              </div>
-              :
-              console.log('cray cray')
-            } 
-          </div>
-          <div className="movie-info">
-            <h5>{movie.Title} ({movie.Year})</h5>
-          </div>
-        </div>
-      ))}
+      {movieList}
     </>
   )
 }

d0ee8a0aa1f85a510ee5db14bc469bdf74b366e5
johnpmeyers20 <johnpmeyers20@yahoo.com>
2021-05-08 15:31:48 -0400

    added nominees === null || statement to movieList component

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -4,13 +4,13 @@ import React from 'react';
 const MovieList = (props) => {
   const AddNominee = props.addNominee;
   const nominees = props.nominees;
+  console.log(nominees);
 
   const movieList = props.movies.map((movie, index) => (
     <div className="container" key={movie.imdbID}>
       <div className="image-container d-flex justify-content-center m-1">
         <img src={movie.Poster} alt="movie" />
-        {console.log('noms4ya', nominees)}
-        { nominees.length < 5 ?
+        { nominees === null || nominees.length < 5 ?
           <div
             onClick={() => props.handleNominee(movie)}
             className="overlay d-flex align-items-center justify-content-center">
